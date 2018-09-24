769353e9371c7f69752afaedfe868a11f1435379
alecsmith84 <astonmartindbrs9>
2018-09-24 15:17:03 -0500

    Made a change to check branch

--- app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
@@ -37,6 +37,8 @@ public class DumbMovieContent {
     private static final String actionImage = "genericaction";
     private static final String actionWeblink = "http://www.denofgeek.com/us/movies/18824/the-5-most-common-action-movie-plots";
 
+    /*comment to make a change*/
+
 
     /**
      * Create and return an array of Movie items.  Duh!
