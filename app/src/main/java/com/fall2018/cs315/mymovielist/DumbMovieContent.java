28d44551935eaad7b997ed72761a1b2b61c68080
microphone0 <ams17a@acu.edu>
2018-09-24 15:17:06 -0500

    Make that change, check that branch

--- app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
@@ -37,6 +37,7 @@ public class DumbMovieContent {
     private static final String actionImage = "genericaction";
     private static final String actionWeblink = "http://www.denofgeek.com/us/movies/18824/the-5-most-common-action-movie-plots";
 
+    // Make a change to commit and push
 
     /**
      * Create and return an array of Movie items.  Duh!
