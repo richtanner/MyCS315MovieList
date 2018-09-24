0bb5e7d2b9d431b671168d302bff2dfc45ab46ee
abigailverhelle <Arv16a@acu.edu>
2018-09-24 15:17:14 -0500

    Checking to see if this works!!!

--- app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
@@ -37,7 +37,7 @@ public class DumbMovieContent {
     private static final String actionImage = "genericaction";
     private static final String actionWeblink = "http://www.denofgeek.com/us/movies/18824/the-5-most-common-action-movie-plots";
 
-
+// test for commit and push 
     /**
      * Create and return an array of Movie items.  Duh!
      */
