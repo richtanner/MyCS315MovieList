54933f5d82f0500973a018f81b4ca179a938aba6
Jessica <jeh15g@acu.edu>
2018-09-26 15:40:47 -0500

    Added 5 movies

--- app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
@@ -27,17 +27,35 @@ public class DumbMovieContent {
     // CS315: DO THIS
     // TODO: Create five NEW movie objects here. Complete with images and URLs. DELETE the two existing movies, they are only here as an example
 
-    private static final String movie1Title = "Generic Rom Com";
-    private static final String movie1Description = "Naturally, focuses on 2 people who really shouldn’t be together. It somehow becomes mutually advantageous for them to be together, or perhaps, they are interested in each other but their romance is based on false pretenses. Because the leads generally have no charisma, they each need a zany best friend who inserts dirty jokes wherever necessary. The two leads will fall in love in an extended music montage set to the music of Sixpence None the Richer. Once they find out about the false pretenses, they will take some time apart but realize how much they care for the other. Then comes “The Chase,” which can either be in an airport (not legal anymore), in traffic or somehow on boats. \n \nYou've seen this before, but not with these two actors!";
-    private static final String movie1Year = "2015";
-    private static final String movie1Image = "genericromcom";
-    private static final String movie1Weblink = "http://showtimeshowdown.com/5-cliche-formulas-of-movie-genres/";
+    private static final String movie1Title = "Spider-Man Homecoming";
+    private static final String movie1Description = "Peter Parker balances his life as an ordinary high school student in Queens with his superhero\nalter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New\nYork City.";
+    private static final String movie1Year = "2017";
+    private static final String movie1Image = "spider-man";
+    private static final String movie1Weblink = "https://www.imdb.com/title/tt2250912/";
 
-    private static final String actionTitle = "Every Action Movie Ever";
-    private static final String actionDescription = "The villain has left the hero for dead, or killed the hero’s brother, sister, parents, wife or family pets. Filled with righteous fury, the hero tools up and embarks on a bloody rampage.  \n \nIf the movie does well, we will do it again in the sequel! \n \nMaybe the villain tries to get revenge on the hero for getting revenge on the villain in this movie...";
-    private static final String actionYear = "2017";
-    private static final String actionImage = "genericaction";
-    private static final String actionWeblink = "http://www.denofgeek.com/us/movies/18824/the-5-most-common-action-movie-plots";
+    private static final String movie2Title = "Avengers: Infinity War";
+    private static final String movie2Description = "The Avengers and their allies must be willing to sacrifice all in an\nattempt to defeat the powerful Thanos before his blitz of\ndevastation and ruin puts an end to the universe.";
+    private static final String movie2Year = "2018";
+    private static final String movie2Image = "infinity-war";
+    private static final String movie2Weblink = "https://www.imdb.com/title/tt4154756/";
+
+    private static final String movie3Title = "Captain America: Civil War";
+    private static final String movie3Description = "Political involvement in the Avengers' activities causes a rift between Captain America and\nIron Man.";
+    private static final String movie3Year = "2016";
+    private static final String movie3Image = "civil-war";
+    private static final String movie3Weblink = "https://www.imdb.com/title/tt3498820/";
+
+    private static final String movie4Title = "Guardians of the Galaxy";
+    private static final String movie4Description = "A group of intergalactic criminals are forced to work together to stop a fanatical warrior from\ntaking control of the universe.";
+    private static final String movie4Year = "2014";
+    private static final String movie4Image = "gotg";
+    private static final String movie4Weblink = "https://www.imdb.com/title/tt2015381/";
+
+    private static final String movie5Title = "Thor: Ragnarok";
+    private static final String movie5Description = "Thor is imprisoned on the planet Sakaar, and must race against time to return to Asgard and\nstop Ragnarök, the destruction of his world, at the hands of the powerful and ruthless\nvillain Hela.";
+    private static final String movie5Year = "2017";
+    private static final String movie5Image = "thor-ragnarok";
+    private static final String movie5Weblink = "https://www.imdb.com/title/tt3501632/";
 
 
     /**
@@ -46,12 +64,18 @@ public class DumbMovieContent {
     public List<MovieModel> createMovieMagic () {
 
         // make those movie objects
-        MovieModel action = new MovieModel(actionTitle, actionDescription, actionYear, actionImage, actionWeblink);
-        MovieModel romcom = new MovieModel(movie1Title, movie1Description, movie1Year, movie1Image, movie1Weblink);
+        MovieModel movie1 = new MovieModel(movie1Title, movie1Description, movie1Year, movie1Image, movie1Weblink);
+        MovieModel movie2 = new MovieModel(movie2Title, movie2Description, movie2Year, movie2Image, movie2Weblink);
+        MovieModel movie3 = new MovieModel(movie3Title, movie3Description, movie3Year, movie3Image, movie3Weblink);
+        MovieModel movie4 = new MovieModel(movie4Title, movie4Description, movie4Year, movie4Image, movie4Weblink);
+        MovieModel movie5 = new MovieModel(movie5Title, movie5Description, movie5Year, movie5Image, movie5Weblink);
 
         // add EACH movie object to our lists and maps
-        addMovieToList(action);
-        addMovieToList(romcom);
+        addMovieToList(movie1);
+        addMovieToList(movie2);
+        addMovieToList(movie3);
+        addMovieToList(movie4);
+        addMovieToList(movie5);
 
         // no more movies to add?  Okay... return our list
         return MOVIES;
