0c7ac711462df3d60af1ff061dba51b3b3811037
Zane Burton <zmb14a@acu.edu>
2018-10-03 17:22:30 -0500

    Please work lol

--- app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
@@ -11,6 +11,7 @@ public class DumbMovieContent {
      * A map of the Movie items, by ID (title).
      */
     public static final Map<String, MovieModel> ITEM_MAP = new HashMap<String, MovieModel>();
+    public int count = 0;
 
     /**
      * A List of the Movie items.
@@ -25,17 +26,37 @@ public class DumbMovieContent {
     // CS315: DO THIS
     // TODO: Create five NEW movie objects here. Complete with images and URLs. DELETE the two existing movies, they are only here as an example
 
-    private static final String movie1Title = "Generic Rom Com";
-    private static final String movie1Description = "Naturally, focuses on 2 people who really shouldn’t be together. It somehow becomes mutually advantageous for them to be together, or perhaps, they are interested in each other but their romance is based on false pretenses. Because the leads generally have no charisma, they each need a zany best friend who inserts dirty jokes wherever necessary. The two leads will fall in love in an extended music montage set to the music of Sixpence None the Richer. Once they find out about the false pretenses, they will take some time apart but realize how much they care for the other. Then comes “The Chase,” which can either be in an airport (not legal anymore), in traffic or somehow on boats. \n \nYou've seen this before, but not with these two actors!";
-    private static final String movie1Year = "2015";
-    private static final String movie1Image = "genericromcom";
-    private static final String movie1Weblink = "http://showtimeshowdown.com/5-cliche-formulas-of-movie-genres/";
+    private static final String movie1Title = "How to lose a guy in 10 days";
+    private static final String movie1Description = "Benjamin Barry is an advertising executive and ladies' man who, to win a big campaign, bets that he can make a woman fall in love with him in 10 days. Andie Anderson covers the \"How To\" beat for \"Composure\" magazine and is assigned to write an article on \"How to Lose a Guy in 10 days.\" ";
+    private static final String movie1Year = "2013";
+    private static final String movie1Image = "romcom";
+    private static final String movie1Weblink = "https://www.imdb.com/title/tt0251127/";
 
-    private static final String actionTitle = "Every Action Movie Ever";
-    private static final String actionDescription = "The villain has left the hero for dead, or killed the hero’s brother, sister, parents, wife or family pets. Filled with righteous fury, the hero tools up and embarks on a bloody rampage.  \n \nIf the movie does well, we will do it again in the sequel! \n \nMaybe the villain tries to get revenge on the hero for getting revenge on the villain in this movie...";
-    private static final String actionYear = "2017";
-    private static final String actionImage = "genericaction";
-    private static final String actionWeblink = "http://www.denofgeek.com/us/movies/18824/the-5-most-common-action-movie-plots";
+    private static final String actionTitle = "LOTR";
+    private static final String actionDescription = "The future of civilization rests in the fate of the One Ring, which has been lost for centuries. Powerful forces are unrelenting in their search for it. \n But fate has placed it in the hands of a young Hobbit named Frodo Baggins (Elijah Wood), who inherits the Ring and steps into legend. \nA daunting task lies ahead for Frodo when he becomes the Ringbearer - to destroy the One Ring in the fires of Mount Doom where it was forged.";
+    private static final String actionYear = "2001";
+    private static final String actionImage = "Lord";
+    private static final String actionWeblink = "https://www.imdb.com/title/tt0120737/";
+
+    private static final String pixarTitle = "Cars 3";
+    private static final String pixarDescription = "Cars 3 is a 2017 American 3D computer-animated comedy-adventure film produced by Pixar Animation Studios and released by Walt Disney Pictures. ... \n \n In the film, Lightning McQueen sets out to prove to a new generation of high tech race cars that he is still the best race car in the world.";
+    private static final String pixarYear = "2017";
+    private static final String pixarImage = "cars";
+    private static final String pixarWeblink = "https://www.imdb.com/title/tt3606752/plotsummary";
+
+    private static final String lionTitle = "Lion King";
+    private static final String lionDescription = "This Disney animated feature follows the adventures of the young lion Simba (Zoe Leader), the heir of his father, Mufasa (Ernie Sabella). \nSimba's wicked uncle, Scar (Rowan Atkinson), plots to usurp Mufasa's throne by luring father and son into a stampede of wildebeests. But Simba escapes, and only Mufasa is killed. \n Simba returns as an adult (Jeremy Irons) to take back his homeland from Scar with the help of his friends Timon (Jonathan Taylor Thomas) and Pumbaa (Cheech Marin).";
+    private static final String lionYear = "1994";
+    private static final String lionImage = "lion";
+    private static final String lionWebLink = "https://www.imdb.com/title/tt0110357/";
+
+    private static final String spaceTitle = "Interstellar";
+    private static final String spaceDescription = "In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable.\n Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth's population to a new home via a wormhole. \nBut first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind's new home.";
+    private static final String spaceYear = "2014";
+    private static final String spaceImage = "space";
+    private static final String spaceWeblink = "https://www.imdb.com/title/tt0816692/";
+
+
 
 
     /**
@@ -46,11 +67,19 @@ public class DumbMovieContent {
         // make those movie objects
         MovieModel action = new MovieModel(actionTitle, actionDescription, actionYear, actionImage, actionWeblink);
         MovieModel romcom = new MovieModel(movie1Title, movie1Description, movie1Year, movie1Image, movie1Weblink);
+        MovieModel pixar = new MovieModel(pixarTitle, pixarDescription, pixarYear, pixarImage, pixarWeblink);
+        MovieModel space = new MovieModel(spaceTitle, spaceDescription, spaceYear, spaceImage, spaceWeblink);
+        MovieModel lion = new MovieModel(lionTitle, lionDescription, lionYear, lionImage, lionWebLink);
 
         // add EACH movie object to our lists and maps
-        addMovieToList(action);
-        addMovieToList(romcom);
-
+        if (count <= 0) {
+            addMovieToList(action);
+            addMovieToList(romcom);
+            addMovieToList(pixar);
+            addMovieToList(space);
+            addMovieToList(lion);
+            count++;
+        }
         // no more movies to add?  Okay... return our list
         return MOVIES;
     }
--- app/src/main/java/com/fall2018/cs315/mymovielist/MovieDetailFragment.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/MovieDetailFragment.java
@@ -1,6 +1,9 @@
 package com.fall2018.cs315.mymovielist;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -10,7 +13,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.graphics.Bitmap;
+
+import java.net.URI;
+import java.net.URL;
 
 /**
  * A fragment representing a single Movie detail screen.
@@ -24,11 +33,12 @@ public class MovieDetailFragment extends Fragment {
      * represents.
      */
     public static final String ARG_ITEM_ID = "item_id";
-
+    private ProgressBar spinner;
     /**
      * The Movie content this fragment is presenting.
      */
     private MovieModel mItem;
+    public String imageName;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -41,8 +51,11 @@ public class MovieDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         // CS315: DO THIS
         // TODO: BUG FIX - Figure out why the App CRASHES when we rotate this Activity...
+
+
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             // Load the Movie content specified by the fragment
@@ -56,15 +69,43 @@ public class MovieDetailFragment extends Fragment {
             if (appBarLayout != null) {
                 appBarLayout.setTitle(mItem.getMovieTitle());
             }
-            ImageView thisMovieImageView = activity.findViewById(R.id.movieImageView);
+            final ImageView thisMovieImageView = activity.findViewById(R.id.movieImageView);
             if (thisMovieImageView != null) {
 
                 // CS315: DO THIS
                 // TODO: Set the image based upon the string we got stashed in getMovieImage()
+                if (mItem.getMovieTitle() == "Cars 3") {
+                    thisMovieImageView.setImageResource(R.drawable.cars);
+                }
 
+                if (mItem.getMovieTitle() == "How to lose a guy in 10 days") {
+                    thisMovieImageView.setImageResource(R.drawable.romcom);
+                }
+                if (mItem.getMovieTitle() == "LOTR") {
+                    thisMovieImageView.setImageResource(R.drawable.lord);
+                }
+                if (mItem.getMovieTitle() == "Lion King") {
+                    thisMovieImageView.setImageResource(R.drawable.lion);
+                }
+                if (mItem.getMovieTitle() == "Interstellar") {
+                    thisMovieImageView.setImageResource(R.drawable.space);
+                }
             }
 
             FloatingActionButton fab = (FloatingActionButton) activity.findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String url = mItem.getMovieWeblink();
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                }
+            });
+
+
+
+           /* FloatingActionButton fab = (FloatingActionButton) activity.findViewById(R.id.fab);
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -76,7 +117,7 @@ public class MovieDetailFragment extends Fragment {
                     Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
-            });
+            });*/
         }
     }
 
