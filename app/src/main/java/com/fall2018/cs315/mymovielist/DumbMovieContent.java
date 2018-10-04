17ad1c848f09086e7ec622586580f06b69b379f9
Michael <jmw14b@acu.edu>
2018-10-04 10:26:02 -0500

    art update

--- app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
@@ -40,31 +40,31 @@ public class DumbMovieContent {
     private static final String horrorTitle = "Every Horror Flick Ever";
     private static final String horrorDescription = "starts off with any group of friends whom are expressively different in the beginning but in reality share one thing in common..stupidity. They go to some spooky place where some event happened that is still present in the place.  \n \nThe movie is a one hit generic series of pictures youll see again only with a different monster!";
     private static final String horrorYear = "2018";
-    private static final String horrorImage = "genericaction";
+    private static final String horrorImage = "horror_poster";
     private static final String horrorWeblink ="https://www.quora.com/Why-are-horror-movies-so-predictable";
 
     private static final String sitcomTitle = "Every Sitcom Ever";
     private static final String sitcomDescription = "starring at least one celebrity that cant get another movie to act in, this is the daily ups and downs of a group of friends or family enclosed around a single location that are laughed at for something subjectively funny.";
     private static final String sitcomYear = "1989";
-    private static final String sitcomImage = "genericaction";
+    private static final String sitcomImage = "jerry";
     private static final String sitcomWeblink ="https://www.quora.com/Why-are-sitcoms-so-unfunny";
 
     private static final String animeTitle = "Every Anime Ever";
     private static final String animeDescription = "the story begins with the main character as a rookie amongst the vast amount of characters that are stronger and unique at first but from the first fight to the end of the story will proceed to all admire the main character's clout. Some episodes contain nothing but powering up and the main character evolves again, and again, and again. \n \nWill our hero defeat whatever villian it is that has some sort of maniacal reputation? find out a few episodes later on every anime ever!";
     private static final String animeYear = "1984";
-    private static final String animeImage = "genericaction";
+    private static final String animeImage = "coryinthemanga";
     private static final String animeWeblink ="https://www.animenewsnetwork.com/bbs/phpBB2/viewtopic.php?t=41317";
 
     private static final String scifiTitle = "Every Sci-Fi Ever";
     private static final String scifiDescription = "we start with the military or scientist organization discovering signs of life beyond our own beings. A group of senseless humans that are about as alien as the aliens themselves make dumb decisions leading this expedition to a complete disaster. With an us vs them mentality, the organization also makes really dumb decisions which cost countless lives to the outside forces, But you will still watch it cause you can look at the cool things and is a step up from horror movies!";
     private static final String scifiYear = "1902";
-    private static final String scifiImage = "genericaction";
+    private static final String scifiImage = "spaceballs";
     private static final String scifiWeblink ="https://us.blastingnews.com/opinion/2017/08/sci-fi-movies-predictable-and-superficial-001923681.html";
 
     private static final String superheroTitle = "Every Super Hero Movie Ever";
     private static final String superheroDescription = "featuring uniconic orchestra music or unoriginal songs used in the movie, our hero is pretty extrordinary and seems to be at their best only to get sacked by some opponent, only then will he/she pander to becoming more people like so that he/she can be more relatable to the audience as they give it a secondtry against the opposition only to win but leave the city in complete disarray.";
     private static final String superheroYear = "2008";
-    private static final String superheroImage = "genericaction";
+    private static final String superheroImage = "superheroes";
     private static final String superheroWeblink ="https://www.quora.com/Why-are-superhero-movies-so-predictable";
 
     /**
