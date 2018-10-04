f5da01d2586707028b0f3cf577eeac6dc81a258d
Jessica <jeh15g@acu.edu>
2018-10-03 22:54:29 -0500

    struggling with the loading screen >:|

--- app/src/main/java/com/fall2018/cs315/mymovielist/MovieWebpage.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/MovieWebpage.java
@@ -2,7 +2,9 @@ package com.fall2018.cs315.mymovielist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MovieWebpage extends AppCompatActivity{
 
@@ -17,6 +19,53 @@ public class MovieWebpage extends AppCompatActivity{
         String url = getIntent().getStringExtra(WEB_URL);
 
         WebView web = (WebView) findViewById(R.id.webView);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //hide loading image
+                findViewById(R.id.imageLoading1).setVisibility(View.GONE);
+                //show webview
+                findViewById(R.id.webView).setVisibility(View.VISIBLE);
+            }
+        });
+
         web.loadUrl(url);
     }
 }
+
+/*
+web.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //hide loading image
+                findViewById(R.id.imageLoading1).setVisibility(View.GONE);
+                //show webview
+                findViewById(R.id.webView).setVisibility(View.VISIBLE);
+            }
+
+
+        });
+ */
+
+/*
+        WebView wv = (WebView) findViewById(R.id.webView1);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new WebViewClient() {
+
+            ...
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //hide loading image
+                findViewById(R.id.imageLoading1).setVisibility(View.GONE);
+                //show webview
+                findViewById(R.id.webView1).setVisibility(View.VISIBLE);
+            }
+
+
+        });
+        wv.loadUrl("http://yoururlhere.com");
+*/
