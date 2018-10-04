84857ba63ce42df5bb2610a85eb98106d4e8fd5f
Lucas Newton <ldn15a@acu.edu>
2018-10-03 22:58:02 -0500

    fix file names and change variable names reflect name changes

--- app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
+++ app/src/main/java/com/fall2018/cs315/mymovielist/DumbMovieContent.java
@@ -28,14 +28,14 @@ public class DumbMovieContent {
     private static final String movie0Title = "Star Wars: Episode V The Empire Strikes Back";
     private static final String movie0Description = "It's a Star Wars movie made before Disney took over in the 21st century. It is sci-fi in case you didn't know and if you haven' heard of Star Wars then....have you been living under a rock?";
     private static final String movie0Year = "1980";
-    private static final String movie0Image = "starWarsEmpireStrikesBack";
+    private static final String movie0Image = "star_wars_strikes_back";
     private static final String movie0WebLink = "https://www.starwars.com/films/star-wars-episode-v-the-empire-strikes-back";
 
     private static final String movie1Title = "Generic Rom Com";
     private static final String movie1Description = "Naturally, focuses on 2 people who really shouldn’t be together. It somehow becomes mutually advantageous for them to be together, or perhaps, they are interested in each other but their romance is based on false pretenses. Because the leads generally have no charisma, they each need a zany best friend who inserts dirty jokes wherever necessary. The two leads will fall in love in an extended music montage set to the music of Sixpence None the Richer. Once they find out about the false pretenses, they will take some time apart but realize how much they care for the other. Then comes “The Chase,” which can either be in an airport (not legal anymore), in traffic or somehow on boats. \n \nYou've seen this before, but not with these two actors!";
     private static final String movie1Year = "2015";
     private static final String movie1Image = "genericromcom";
-    private static final String movie1Weblink = "http://showtimeshowdown.com/5-cliche-formulas-of-movie-genres/";
+    private static final String movie1WebLink = "http://showtimeshowdown.com/5-cliche-formulas-of-movie-genres/";
 
     /*private static final String movie2Title = "Star Wars ":
     private static final String movie2Description = "";
@@ -61,12 +61,12 @@ public class DumbMovieContent {
     public List<MovieModel> createMovieMagic () {
 
         // make those movie objects
-        MovieModel action = new MovieModel(actionTitle, actionDescription, actionYear, actionImage, actionWeblink);
-        MovieModel romcom = new MovieModel(movie1Title, movie1Description, movie1Year, movie1Image, movie1Weblink);
+        MovieModel romcom0 = new MovieModel(movie0Title, movie0Description, movie0Year, movie0Image, movie0WebLink);
+        MovieModel romcom1 = new MovieModel(movie1Title, movie1Description, movie1Year, movie1Image, movie1WebLink);
 
         // add EACH movie object to our lists and maps
-        addMovieToList(action);
-        addMovieToList(romcom);
+        addMovieToList(romcom0);
+        addMovieToList(romcom1);
 
         // no more movies to add?  Okay... return our list
         return MOVIES;
