55a4b178334d5dd25fa89d20cdcf9c1cb2137b46
TravisTowell <37230402+TravisTowell@users.noreply.github.com>
2018-10-03 23:15:34 -0500

    next commit

---
+++ app/src/main/java/com/fall2018/cs315/mymovielist/WebView.java
@@ -0,0 +1,15 @@
+
+package com.fall2018.cs315.mymovielist;
+
+import andriod.os.Bundle;
+import andriod.support.v7.AppCompatActivity;
+
+public class WebView extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.WebView);
+  }
+}
